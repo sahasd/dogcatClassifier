21db100499691d15268c4ee44d7be95d304806e6
sahasd <sahasd@gmail.com>
2017-06-27 15:23:25 -0700

    added data files and init classifier

---
+++ petClassifier.py
@@ -0,0 +1,22 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import numpy as np
+
+img_width, img_height = 150,150
+train_dir = './data/train'
+test_dir  = './data/test'
+
+img = load_img('data/train/cats/cat.0.jpg')  # this is a PIL image
+x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+i = 0
+for batch in datagen.flow(x, batch_size=1, save_to_dir='preview', save_prefix='cat', save_format='jpeg'):
+    i += 1
+    if i > 20:
+        break 
